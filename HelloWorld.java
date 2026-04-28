24f4b20b25d9f8b511e85516f8b7489a72882bdc
horlartundhey <horlartundhey@gmail.com>
2026-04-28 11:27:57 +0100

    task 1 submission

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+    public static void main(String[] args)
+{
+    String name = "Olatunde";
+    String City = "Lagos";
+    String reason = "I am learning JAVA ro become a much better developer, that builds solutions to businesses";
+
+
+
+    System.out.println("Hello, World!");
+    System.out.println("My name is " + name);
+    System.out.println("I am from " + City);
+    System.out.println(reason);
+
+}}
